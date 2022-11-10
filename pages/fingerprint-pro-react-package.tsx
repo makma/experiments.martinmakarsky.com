dc038a8c53c24fda002c35116f9f948bdd745aa1
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2022-11-10 12:00:05 +0100

    feat: add a random linkedId to the react lib request

--- pages/fingerprint-pro-react-package.tsx
+++ pages/fingerprint-pro-react-package.tsx
@@ -37,7 +37,7 @@ const FingerprintData: NextPage = () => {
   }
 
   const { isLoading, error, data, getData } = useVisitorData(
-    { extendedResult, products },
+    { extendedResult, products, linkedId: "someRandomLinkedId" },
     { immediate: true }
   );
 
