7677240ad4b056ed3e371954e378faede1d82cc2
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2023-04-11 14:03:03 +0200

    fix: reading environment variables

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,5 +1,5 @@
 export const FINGERPRINT_PUBLIC_API_KEY: string = "tQUwQQOuG9TNwqc6F4I2";
-export const FINGERPRINT_SECRET_API_KEY: string = process.env.FINGERPRINT_SECRET_API_KEY ?? ''
+export const FINGERPRINT_SECRET_API_KEY: string = env.FINGERPRINT_SECRET_API_KEY ?? ''
 
 export const FINGERPRINT_PUBLIC_API_KEY_ZERO_TRUST: string = "xI1tKWIHK6UGTshR8vDc"; // BOTD + ZERO TRUST application
 export const CUSTOM_SUBDOMAIN: string = "https://fp.martinmakarsky.com";
