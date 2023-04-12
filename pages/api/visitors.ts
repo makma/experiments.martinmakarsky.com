57f233d08d34f78d30f8f2112e9aa4baaab86222
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2023-04-12 12:00:58 +0200

    fix: try to hack the API key from environment variable

--- pages/api/visitors.ts
+++ pages/api/visitors.ts
@@ -1,13 +1,12 @@
 import type { NextRequest } from 'next/server'
-import { FINGERPRINT_SECRET_API_KEY } from '../../shared/constants'
 
 export const config = {
   runtime: 'edge',
 }
 
 export default async function handler(req: NextRequest) {
+  const apiKey = process.env.FINGERPRINT_SECRET_API_KEY ?? ''
   const visitorId = 'T6X2T5TSnZTeSUNZ1oBQ'
-  const apiKey = FINGERPRINT_SECRET_API_KEY
   const requestId = '1681130720291.YHbcpl'
 
   const fingerprintJSProServerApiUrl = new URL(
