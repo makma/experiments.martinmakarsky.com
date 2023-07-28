ae808d591f2a037cd9a76d860229cf321506fc27
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2023-07-28 16:28:16 +0200

    feat: experiment - put bot event info to the KV storage

---
+++ pages/api/fingerprint-webhook-listener.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { KVNamespace } from "@cloudflare/workers-types";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const body: any = req.body;
+        const webhookPayload = await body.json();
+
+        const { BOT_EVENTS } = (process.env as unknown as { BOT_EVENTS: KVNamespace });
+        BOT_EVENTS.put(webhookPayload.requestId, webhookPayload);
+
+        // Send a response back to the sender (optional)
+        res.status(200).json({ message: 'Webhook data received successfully.' });
+        res.end();
+    } else {
+        res.status(405).end();
+    }
+}
