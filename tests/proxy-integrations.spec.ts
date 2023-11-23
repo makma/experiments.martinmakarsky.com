147f5cdf80e1b0ed5843c53247698f37cb3957a9
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2023-11-23 12:27:37 +0100

    feat: add playwright tests

---
+++ tests/proxy-integrations.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+
+const baseDomain = "https://experiments.martinmakarsky.com"
+
+test('Cloudflare Proxy Integration returns the visitorId', async ({ page }) => {
+  await page.goto(`${baseDomain}/fingerprint-pro-react-cloudflare`);
+
+  const preSelector = 'pre';
+
+  await page.waitForSelector(preSelector);
+  const preElement = await page.$(preSelector);
+  const preText = await preElement.textContent();
+
+  expect(preText).toContain('\"visitorId\":');
+});
+
+test('Azure Proxy Integration returns the visitorId', async ({ page }) => {
+  await page.goto(`${baseDomain}/fingerprint-pro-react-azure`);
+
+  const preSelector = 'pre';
+
+  await page.waitForSelector(preSelector);
+  const preElement = await page.$(preSelector);
+  const preText = await preElement.textContent();
+
+  expect(preText).toContain('\"visitorId\":');
+});
+
+test('CloudFront Proxy Integration returns the visitorId', async ({ page }) => {
+  await page.goto(`${baseDomain}/fingerprint-pro-react-cloudfront`);
+
+  const preSelector = 'pre';
+
+  await page.waitForSelector(preSelector);
+  const preElement = await page.$(preSelector);
+  const preText = await preElement.textContent();
+
+  expect(preText).toContain('\"visitorId\":');
+});
+
+test('Akamai Proxy Integration returns the visitorId', async ({ page }) => {
+  await page.goto(`${baseDomain}/fingerprint-pro-react-akamai`);
+
+  const preSelector = 'pre';
+
+  await page.waitForSelector(preSelector);
+  const preElement = await page.$(preSelector);
+  const preText = await preElement.textContent();
+
+  expect(preText).toContain('\"visitorId\":');
+});
