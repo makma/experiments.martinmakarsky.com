d656188eb9931136fdd687661bac9da54bf500d6
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-01-22 14:32:08 +0100

    test: add tests for the Fastly VCL Proxy integration

--- tests/proxy-integrations.spec.ts
+++ tests/proxy-integrations.spec.ts
@@ -61,3 +61,15 @@ test('Custom Proxy Integration returns the visitorId', async ({ page }) => {
 
   expect(preText).toContain('\"visitorId\":');
 });
+
+test('Fastly VCL Proxy Integration returns the visitorId', async ({ page }) => {
+  await page.goto(`${baseDomain}/fingerprint-pro-fastly-vcl`);
+
+  const preSelector = 'pre';
+
+  await page.waitForSelector(preSelector, { timeout: 10 * 1000 });
+  const preElement = await page.$(preSelector);
+  const preText = await preElement.textContent();
+
+  expect(preText).toContain('\"visitorId\":');
+});
