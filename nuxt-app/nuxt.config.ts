355f57c106a25b632d6c9a39c32934992cf5c33c
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-01-25 09:12:39 +0100

    feat: configure static vue

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
   runtimeConfig: {
     public: {
       // Inject Fingerprint Pro API key
