efcf5d763f155c21046c1e5a5239274fab71aa20
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-01-25 11:15:55 +0100

    fix: set base URL for the nuxt project

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   target: 'static',
+  app: {
+    baseURL: '/nuxt/'
+  },
   runtimeConfig: {
     public: {
       // Inject Fingerprint Pro API key
