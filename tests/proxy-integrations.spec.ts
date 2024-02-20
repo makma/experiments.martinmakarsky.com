4e7b4d2e0b794e13a0bb78a785b3cfb35c328974
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-02-20 15:11:31 +0100

    test: add tests for the custom subdomain

--- tests/proxy-integrations.spec.ts
+++ tests/proxy-integrations.spec.ts
@@ -73,3 +73,15 @@ test('Fastly VCL Proxy Integration returns the visitorId', async ({ page }) => {
 
   expect(preText).toContain('\"visitorId\":');
 });
+
+test('Custom subdomain returns the visitorId', async ({ page }) => {
+  await page.goto(`${baseDomain}/fingerprint-pro-and-botd-full-subdomain`);
+
+  const preSelector = 'pre';
+
+  await page.waitForSelector(preSelector, { timeout: 10 * 1000 });
+  const preElement = await page.$(preSelector);
+  const preText = await preElement.textContent();
+
+  expect(preText).toContain('\"visitorId\":');
+});
