8db3a30f5e1ea5e059e35fdee7bcddee6579c201
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-02-29 10:46:50 +0100

    fix: remove log from a test

--- tests/server-api.spec.ts
+++ tests/server-api.spec.ts
@@ -5,12 +5,10 @@ const baseDomain = "https://experiments.martinmakarsky.com"
 test('Server API returns a response', async ({ page }) => {
   await page.goto(`${baseDomain}/fingerprint-pro-cloudflare-server-api-sdk`);
 
-  const preSelector = 'pre';
   const serverApiResponseElement = page.locator("#server-api-response");
   await serverApiResponseElement.waitFor({state: "visible"}, { timeout: 10 * 1000 })
 
   const serverApiText = await serverApiResponseElement.textContent();
-  console.log(serverApiText);
 
   expect(serverApiText).toContain('\"visitorId\":');
 });
