24ebe05f0a208bb8313bebe2cea36d803b54c58b
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-05-20 12:48:41 +0200

    fix: NextResponse instead of Response

--- next-app/app/delete-by-visitorId/delete/route.ts
+++ next-app/app/delete-by-visitorId/delete/route.ts
@@ -1,13 +1,15 @@
 import { DecryptionAlgorithm, unsealEventsResponse } from '@fingerprintjs/fingerprintjs-pro-server-api';
+import { NextResponse } from 'next/server';
+
 
 export async function DELETE(request: Request) {
   if (request.method !== "DELETE") {
-    return Response.json({ message: `${request.method} is not a supported method.` }, { status: 400 });
+    return NextResponse.json({ message: `${request.method} is not a supported method.` }, { status: 400 });
   }
 
   const SEALED_RESULTS_DECRYPTION_KEY = process.env.SEALED_RESULTS_DECRYPTION_KEY;
   if (!SEALED_RESULTS_DECRYPTION_KEY) {
-    return Response.json({ message: `Missing SEALED_RESULTS_DECRYPTION_KEY environment variable` }, { status: 500 });
+    return NextResponse.json({ message: `Missing SEALED_RESULTS_DECRYPTION_KEY environment variable` }, { status: 500 });
   }
 
   const body = await request.json();
@@ -15,28 +17,28 @@ export async function DELETE(request: Request) {
   const unsealedResult = await unsealViaSDK(body.sealedFingerprintData.sealedResult, key);
 
   if (!unsealedResult || !unsealedResult.products.identification || !unsealedResult.products.identification.data) {
-    return Response.json({ message: `Invalid unsealed data.` }, { status: 400 });
+    return NextResponse.json({ message: `Invalid unsealed data.` }, { status: 400 });
   }
 
   // Only I can delete the data
   const requestVisitorId = unsealedResult.products.identification.data.visitorId;
   if (requestVisitorId !== "phKwbdoV8NEAGTATwAiT") {
-    return Response.json({ message: `${requestVisitorId} is not allowed to delete data` }, { status: 403 });
+    return NextResponse.json({ message: `${requestVisitorId} is not allowed to delete data` }, { status: 403 });
   }
 
   const visitorIdToDelete = body.visitorIdToDelete;
 
   if (!visitorIdToDelete) {
-    return Response.json({ message: `${visitorIdToDelete} is not a visitorId.` }, { status: 400 });
+    return NextResponse.json({ message: `${visitorIdToDelete} is not a visitorId.` }, { status: 400 });
   }
 
   if (visitorIdToDelete === unsealedResult.products.identification.data.visitorId) {
-    return Response.json({ message: `Cannot delete since the visitorId to delete is the same as the current visitorId.` }, { status: 400 });
+    return NextResponse.json({ message: `Cannot delete since the visitorId to delete is the same as the current visitorId.` }, { status: 400 });
   }
 
   const secretAPIKey = process.env.FINGERPRINT_SEALED_RESULTS_SECRET_API_KEY
   if (!secretAPIKey) {
-    return Response.json({ message: `Missing secret API key.` }, { status: 500 });
+    return NextResponse.json({ message: `Missing secret API key.` }, { status: 500 });
 
   }
 
@@ -52,9 +54,9 @@ export async function DELETE(request: Request) {
   console.log(response.status)
   console.log(response.body)
   if (response.status !== 200) {
-    return Response.json({ message: { status: response.status, body: response.body } }, { status: 500 });
+    return NextResponse.json({ message: { status: response.status, body: response.body } }, { status: 500 });
   }
-  return Response.json({ message: `The visitorId: ${visitorIdToDelete} is scheduled to be deleted.` }, { status: 200 });
+  return NextResponse.json({ message: `The visitorId: ${visitorIdToDelete} is scheduled to be deleted` }, { status: 200 });
 }
 
 async function unsealViaSDK(sealedResultBase64: string, key: Buffer) {
