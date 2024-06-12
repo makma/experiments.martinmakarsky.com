84c31aa31b4521ec73911c1495e085360411882c
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-06-12 14:16:57 +0200

    feat: add CORS

--- dotnet-app/fingerprint-pro-server-api/Program.cs
+++ dotnet-app/fingerprint-pro-server-api/Program.cs
@@ -1,5 +1,19 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://experiments.martinmakarsky.com",
+                                              "http://localhost:3000")
+                                              .AllowAnyHeader()
+                                              .AllowAnyMethod();
+                      });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -9,6 +23,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
