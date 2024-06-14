12543f6a220005310467496f77e35bb22d7308d5
Martin Makarsky <martin.makarsky@fingerprintjs.com>
2024-06-14 19:11:06 +0200

    feat(next-app): replace the delete API call with the SDK

--- next-app/app/delete-by-visitorId/delete/route.ts
+++ next-app/app/delete-by-visitorId/delete/route.ts
@@ -1,4 +1,4 @@
-import { DecryptionAlgorithm, FingerprintJsServerApiClient, Region, unsealEventsResponse } from '@fingerprintjs/fingerprintjs-pro-server-api';
+import { FingerprintJsServerApiClient, Region } from '@fingerprintjs/fingerprintjs-pro-server-api';
 import { NextResponse } from 'next/server';
 
 
@@ -8,39 +8,16 @@ export async function DELETE(request: Request) {
   }
 
   // Only I and the CI tests can delete the data, very naive, do not copy this code, harmless on the testing sub, harmful on prod use
-  const { searchParams } = new URL(request.url)
-  const deletionSecret = searchParams.get('deletionSecret')
+  const body = await request.json();
+  const deletionSecret = body.deletionSecret;
   if (deletionSecret !== process.env.DELETION_SECRET) {
     return NextResponse.json({ message: `You are not allowed to delete data` }, { status: 403 });
-  }
-
-  const SEALED_RESULTS_DECRYPTION_KEY = process.env.SEALED_RESULTS_DECRYPTION_KEY;
-  if (!SEALED_RESULTS_DECRYPTION_KEY) {
-    return NextResponse.json({ message: `Missing SEALED_RESULTS_DECRYPTION_KEY environment variable` }, { status: 500 });
-  }
-
-  const body = await request.json();
-  const key = Buffer.from(SEALED_RESULTS_DECRYPTION_KEY, 'base64')
-  const unsealedResult = await unsealViaSDK(body.sealedFingerprintData.sealedResult, key);
-
-  if (!unsealedResult || !unsealedResult.products.identification || !unsealedResult.products.identification.data) {
-    return NextResponse.json({ message: `Invalid unsealed data.` }, { status: 400 });
   }
 
   const visitorIdToDelete = body.visitorIdToDelete;
 
   if (!visitorIdToDelete) {
     return NextResponse.json({ message: `${visitorIdToDelete} is not a visitorId.` }, { status: 400 });
-  }
-
-  if (visitorIdToDelete === unsealedResult.products.identification.data.visitorId) {
-    return NextResponse.json({ message: `Cannot delete since the visitorId to delete is the same as the current visitorId.` }, { status: 400 });
-  }
-
-  const secretAPIKey = process.env.FINGERPRINT_SEALED_RESULTS_SECRET_API_KEY
-  if (!secretAPIKey) {
-    return NextResponse.json({ message: `Missing secret API key.` }, { status: 500 });
-
   }
 
   const apiKey = process.env.FINGERPRINT_SECRET_API_KEY ?? "";
@@ -57,18 +34,4 @@ export async function DELETE(request: Request) {
   }
 
   return NextResponse.json({ message: `The visitorId: ${visitorIdToDelete} is scheduled to be deleted` }, { status: 200 });
-}
-
-async function unsealViaSDK(sealedResultBase64: string, key: Buffer) {
-  try {
-    const unsealedData = await unsealEventsResponse(Buffer.from(sealedResultBase64, 'base64'), [
-      {
-        key: key,
-        algorithm: DecryptionAlgorithm.Aes256Gcm,
-      },
-    ]);
-    return unsealedData;
-  } catch (e) {
-    console.error(e);
-  }
 }
--- next-app/app/delete-by-visitorId/page.tsx
+++ next-app/app/delete-by-visitorId/page.tsx
@@ -1,32 +1,19 @@
 "use client";
 
 import styles from "../../styles/Home.module.css";
-import { useState, useEffect } from "react";
-import FingerprintJS, { GetResult } from "@fingerprintjs/fingerprintjs-pro";
-import { FINGERPRINT_PUBLIC_API_KEY_SEALED_RESULTS } from "../../shared/constants";
+import { useState } from "react";
 
 export default function FingerprintSealedResultsDirect() {
-  const [sealedFingerprintData, setSealedFingerprintData] = useState<
-    GetResult | string | null
-  >(null);
   const [visitorIdToDelete, setVisitorIdToDelete] = useState<null | string>(
+    null
+  );
+  const [deletionSecret, setDeletionSecret] = useState<null | string>(
     null
   );
   const [deletionResult, setDeletionResult] = useState<null | string>(null);
 
-  useEffect(() => {
-    (async () => {
-      const fpPromise = FingerprintJS.load({
-        apiKey: FINGERPRINT_PUBLIC_API_KEY_SEALED_RESULTS,
-      });
-      const fp = await fpPromise;
-      const data = await fp.get();
-      setSealedFingerprintData(data);
-    })();
-  }, []);
-
   async function deleteDataByVisitorId() {
-    if (sealedFingerprintData && visitorIdToDelete) {
+    if (visitorIdToDelete) {
       const headers = {
         "Content-Type": "application/json",
       };
@@ -34,7 +21,7 @@ export default function FingerprintSealedResultsDirect() {
       const response = await fetch(`/delete-by-visitorId/delete`, {
         method: "DELETE",
         headers: headers,
-        body: JSON.stringify({ sealedFingerprintData, visitorIdToDelete }),
+        body: JSON.stringify({ visitorIdToDelete, deletionSecret }),
       });
       setDeletionResult(await response.json());
     }
@@ -42,10 +29,15 @@ export default function FingerprintSealedResultsDirect() {
 
   return (
     <div className={styles.container}>
-      <h2>visitorId to delete:</h2>
+      <h2>VisitorId to delete:</h2>
       <input
         type="string"
         onChange={(e) => setVisitorIdToDelete(e.target.value)}
+      ></input>
+      <h2>Deletion secret:</h2>
+      <input
+        type="string"
+        onChange={(e) => setDeletionSecret(e.target.value)}
       ></input>
       <button
         className={styles.button}
