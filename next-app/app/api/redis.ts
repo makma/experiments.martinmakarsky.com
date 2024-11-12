f69e8bff3d91e1eb333f7fc344d110f816097a6c
Valentin V <valentin+github@fingerprint.com>
2024-11-12 12:45:08 -0600

    feat: [hackathon-passkey] add support for redis URL

--- next-app/app/api/redis.ts
+++ next-app/app/api/redis.ts
@@ -1,5 +1,8 @@
+// TODO: this is not the right location for this file,
+// I've created it for hackathon purposes only and it can be moved the a proper "lib" location later.
 import { createClient } from 'redis';
-const redis = createClient();
+const REDIS_URL_DEFAULT = 'redis://localhost:6379'
+const redis = createClient({url: process.env.REDIS_URL || REDIS_URL_DEFAULT});
 redis.on('error', err => console.log('Redis Client Error', err));
 (async () => {
     await redis.connect();
