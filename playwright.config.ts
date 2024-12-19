162e86bf8edc86723b5e917cbb5ebe5b97e1526f
Martin Makarsky <martin.makarsky@fingerprint.com>
2024-12-19 10:41:19 +0100

    fix: fix tests by updating playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  retries: 3,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
