470c8fd0e12e01b9858eee491291abfbca9eeb4a
Martin Makarsky <martin.makarsky@fingerprint.com>
2025-03-28 14:18:51 +0100

    fix: agent data

--- next-app/pages/api/on-demand-identification-collect.ts
+++ next-app/pages/api/on-demand-identification-collect.ts
@@ -61,7 +61,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.send({
       status: 200,
       response: {
-        agentData: fpResponse
+        rawFpResponse: fpResponse
       }
     });
   }
