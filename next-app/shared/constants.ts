828ef0e724d69ef1afc947763bfc6d80e69a94cd
Martin Makarsky <martin.makarsky@fingerprint.com>
2025-09-12 15:50:53 +0200

    fix: static agent

---
+++ next-app/shared/constants.ts
@@ -0,0 +1,40 @@
+
+// sub_AHhMVKY0HFYj3W martinmakarsky.com
+export const FINGERPRINT_PUBLIC_API_KEY = "tQUwQQOuG9TNwqc6F4I2";
+export const FINGERPRINT_PUBLIC_API_KEY_SECONDARY = "deo4QJZKMnmvTT1C9Hmt"
+export const FINGERPRINT_PUBLIC_API_KEY_SAMPLEP_ENVIRONMENT = "dzbq3RL5nuUFAmRlsP4R";
+export const CLOUDFLARE_PROXY_INTEGRATION_SCRIPT_URL_PATTERN = "https://martinmakarsky.com/ZBXJUjor56j9EQCU/KAY8SvdbISPMEu6r?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>"
+export const CLOUDFLARE_PROXY_INTEGRATION_ENDPOINT = "https://martinmakarsky.com/ZBXJUjor56j9EQCU/5iUMYiulWYS5IY5P?region=eu"
+export const FINGERPRINT_PUBLIC_API_KEY_SEALED_ENV_AHhMVKY0HFYj3W = "QlQ8ypcQ9XWoB0ow4LVO"
+export const FWALL_ENVIRONEMNT_PUBLIC_KEY = "DbuL8Q4ud00IlpSKahq7"
+
+// sub_8VLhgjoASvOwjH zero trust + botd
+export const FINGERPRINT_PUBLIC_API_KEY_ZERO_TRUST: string = "xI1tKWIHK6UGTshR8vDc";
+export const CLOUDFLARE_PROXY_INTEGRATION_SCRIPT_URL_PATTERN_ZERO_TRUST = "https://martinmakarsky.com/oCORwuSl6Zs1offp/LglHwvok9iLk2DZt?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>"
+export const CLOUDFLARE_PROXY_INTEGRATION_ENDPOINT_ZERO_TRUST = "https://martinmakarsky.com/oCORwuSl6Zs1offp/91J1K94DvrRDwN6G?region=eu"
+
+// sub_Ezq3jYsYhAKyXJ sealed results
+export const FINGERPRINT_PUBLIC_API_KEY_SEALED_RESULTS: string = "BEczWb6HhcD4ORMrP0aA";
+export const CLOUDFLARE_PROXY_INTEGRATION_SCRIPT_URL_PATTERN_SEALED_CLIENT_RESULTS = "https://martinmakarsky.com/lDqYpEg2vyMYwD3l/nbtKmD2GT7ADvYRm?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>"
+export const CLOUDFLARE_PROXY_INTEGRATION_ENDPOINT_SEALED_CLIENT_RESULTS = "https://martinmakarsky.com/lDqYpEg2vyMYwD3l/WGiRx9E6BGi74Eem?region=eu"
+
+// sub_urYq4aadcfypu0 on STAGING
+export const FINGERPRINT_PUBLIC_API_KEY_STAGING: string = "bAW4LdHhP3gVx4kRBzym";
+export const CLOUDFLARE_PROXY_INTEGRATION_SCRIPT_URL_PATTERN_STAGING = "https://martinmakarsky.com/staging-worker-path/agent-script-download-path?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>"
+export const CLOUDFLARE_PROXY_INTEGRATION_ENDPOINT_STAGING = "https://martinmakarsky.com/staging-worker-path/get-result-path"
+
+export const CUSTOM_SUBDOMAIN: string = "https://fp.martinmakarsky.com";
+
+
+// sub_KpQKsqB3pwVKwi open client response
+
+export const OPEN_CLIENT_RESPONSE_PUBLIC_API_KEY = "awHQbFLn4HoIAxxIqZGZ"
+
+// sub_sZjVxPvnExpRUU testing
+export const FINGERPRINT_PUBLIC_API_KEY_TESTING = "KVyN39rM8EUgLgXE9k7A";
+export const CLOUDFLARE_PROXY_INTEGRATION_SCRIPT_URL_PATTERN_TESTING = "https://martinmakarsky.com/ZtgXygaxmTkwiimg/HUDWO3LTWEHoRcUL?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>"
+export const CLOUDFLARE_PROXY_INTEGRATION_ENDPOINT_TESTING = "https://martinmakarsky.com/ZtgXygaxmTkwiimg/7yzX07VysLl7qdxQ?region=us"
+
+export const FINGERPRINT_PUBLIC_API_KEY_TESTING_2 = "GssRiX4z92UKLAD8aJCo";
+export const CLOUDFLARE_PROXY_INTEGRATION_SCRIPT_URL_PATTERN_TESTING_2 = "https://experiments.cfi-fingerprint.com/ZtgXygaxmTkwiimg/HUDWO3LTWEHoRcUL?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>"
+export const CLOUDFLARE_PROXY_INTEGRATION_ENDPOINT_TESTING_2 = "https://experiments.cfi-fingerprint.com/ZtgXygaxmTkwiimg/7yzX07VysLl7qdxQ?region=us"
