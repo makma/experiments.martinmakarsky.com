665cc30a5f443b8041c7959d14cd4d4177f2bd8d
Martin Makarsky <martin.makarsky@fingerprint.com>
2025-12-23 13:19:41 +0100

    feat: add v4 apge

---
+++ next-app/pages/v4/fingerprint-pro-direct.tsx
@@ -0,0 +1,34 @@
+import styles from "../../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import * as Fingerprint from '@fingerprint/agent'
+import { FINGERPRINT_PUBLIC_API_KEY } from "../../shared/constants";
+
+export default function FingerprintProVanillaAgentDirect() {
+  const [fingerprintData, setFingerprintData] = useState<
+    GetResult | string | null
+  >(null);
+
+  useEffect(() => {
+    (async () => {
+      const fp = Fingerprint.start({
+        apiKey: FINGERPRINT_PUBLIC_API_KEY,
+      });
+      const data = await fp.get({
+      });
+      setFingerprintData(data);
+    })();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {fingerprintData ? (
+        <pre className={styles.data}>
+          {JSON.stringify(fingerprintData, null, 2)}
+        </pre>
+      ) : (
+        <h3>Waiting or data...</h3>
+      )}
+    </div>
+  );
+}
+
