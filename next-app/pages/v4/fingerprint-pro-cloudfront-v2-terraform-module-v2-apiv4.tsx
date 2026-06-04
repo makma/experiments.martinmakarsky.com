ca6382060e5b2b766871116502837a39b76030de
Martin Makarsky <martin.makarsky@fingerprint.com>
2026-06-04 16:04:36 +0200

    feat(CloudFrontv4): remove debug info

--- next-app/pages/v4/fingerprint-pro-cloudfront-v2-terraform-module-v2-apiv4.tsx
+++ next-app/pages/v4/fingerprint-pro-cloudfront-v2-terraform-module-v2-apiv4.tsx
@@ -30,13 +30,7 @@ export default function FingerprintProCloudFrontV2ModuleV2ApiV4() {
   return (
     <div className={styles.container}>
       <h1>CloudFront proxy integration v2 — Terraform module v2, API v4</h1>
-      <div>
-        <pre>subId: sub_AHhMVKY0HFYj3W</pre>
-        <pre>env: prod</pre>
-        <pre>endpoint: {CLOUDFRONT_V2_MODULE_V2_APIV4_ENDPOINT}</pre>
-      </div>
-
-      {error && <p style={{ color: "red", marginTop: 16 }}>Error: {error}</p>}
+{error && <p style={{ color: "red", marginTop: 16 }}>Error: {error}</p>}
 
       {fingerprintData ? (
         <>
